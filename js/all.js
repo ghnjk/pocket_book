44f2d14fa02b38e0e75634c950a0a66cfb114135
ghnjk <ghnjk@foxmail.com>
2019-10-04 17:33:25 +0800

    update

--- js/all.js
+++ js/all.js
@@ -117,14 +117,12 @@ function updateAllRecordState(form, state){
 		alert("未知状态");
 		return false;
 	}
-	url="date.php?action=updaterecordAll?state=" + state.toString();
-	alert(url);
 	var r=confirm(msg);
 	if(r==true){
 		$.ajax({
 			type:"POST",
 			dataType: "json",
-			url:"date.php?action=updaterecordAll?state=" + state.toString(),
+			url:"date.php?action=updaterecordAll&state=" + state.toString(),
 			data: $("#del_all").serialize(),
 			success:function(result){
 				var data = '';
