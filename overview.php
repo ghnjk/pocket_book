2e72a4e9e68ca22a9e8358b69e16ffd2a701cae3
ghnjk <ghnjk@foxmail.com>
2019-10-07 13:16:18 +0800

    fix bugs

--- overview.php
+++ overview.php
@@ -8,6 +8,8 @@ if (get("type") == "in"){
     $type_name = "支出";
     $type = 2;
 }
+$sum_1 = 0.0;
+$sum_2 = 0.0;
 ?>
 <div class="table">
     <div class="table-header-group">  
@@ -16,11 +18,16 @@ if (get("type") == "in"){
         </ul>  
     </div>
     <div class="table-row-group">
-        <?php foreach($banklist as $item){ ?>
+        <?php foreach($banklist as $item){ 
+            $total_1 = total_account_sum($item["bankid"], 1, $type);
+            $total_2 = total_account_sum($item["bankid"], 2, $type);
+            $sum_1 += $total_1;
+            $sum_2 += $total_2;
+            ?>
             <ul class="table-row">
                 <li><?php echo $item["bankname"] ?></li>
-                <li><?php echo show_money( total_account_sum($item["bankid"], 1, $type) ); ?></li>
-                <li><?php echo show_money( total_account_sum($item["bankid"], 2, $type) ); ?></li>
+                <li><?php echo show_money( $total_1 ); ?></li>
+                <li><?php echo show_money( $total_2 ); ?></li>
                 <li>
                     <a href="add.php?bankid=<?php echo $item["bankid"] ?>">记账</a>
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -28,6 +35,11 @@ if (get("type") == "in"){
                 </li>
             </ul>
         <?php } ?>
+    </div>
+    <div class="table-header-group">  
+        <ul class="table-row">  
+            <li class="w12p">合计</li><li class="w12p"><?php echo show_money($sum_1); ?></li><li class="w12p"><?php echo show_money($sum_2); ?></li><li class="w22p"></li>
+        </ul>  
     </div>
 </div>
 <?php include_once("footer.php");?>
