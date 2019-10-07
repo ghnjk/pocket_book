581fd660b2b7d1c7e8e7608200708cb1529a0b97
ghnjk <ghnjk@foxmail.com>
2019-10-07 10:46:00 +0800

    fix bugs

--- detail.php
+++ detail.php
@@ -104,7 +104,7 @@ if($s_state != ""){
         <td bgcolor="#EBEBEB">汇总信息：</td>
         <td bgcolor="#EBEBEB">总收入：</td>
         <td bgcolor="#EBEBEB"><?php
-            echo show_money(estimate_sumary($s_starttime,$s_endtime,$userid, $s_bankid, $s_state,1);
+            echo show_money(estimate_sumary($s_starttime,$s_endtime,$userid, $s_bankid, $s_state,1));
         ?></td>
         <td bgcolor="#EBEBEB">总支出</td>
         <td bgcolor="#EBEBEB"><?php
